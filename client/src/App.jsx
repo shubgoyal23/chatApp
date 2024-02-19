16202eb30e7ff3d3503dc90bb89f08579baf65f6
shubgoyal23 <shubgoyal1998@gmail.com>
2024-02-19 21:43:18 +0530

    login and registration fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
 import Auth from "./Auth";
 import MessageApp from "./MessageApp";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { login, logout } from "./store/loginSlice";
 
 function App() {
    const loginStatus = useSelector((state) => state.login);
+   const dispatch = useDispatch();
+   useEffect(() => {
+      fetch("/api/v1/users/user")
+         .then((res) => res.json())
+         .then((data) => {
+            if (data) {
+               dispatch(login(data.data));
+            } else {
+               dispatch(logout());
+            }
+         })
+         .catch((error) => console.log(error));
+   }, []);
 
    return loginStatus.isLoggedin ? <MessageApp /> : <Auth />;
 }
