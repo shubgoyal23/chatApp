094c1db646a8c5cb87ae805f599feaa0f4dcbb74
shubgoyal23 <shubgoyal1998@gmail.com>
2024-02-29 19:14:05 +0530

    error handler fix

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -49,7 +49,7 @@ router.use((err, req, res, next) => {
        console.error(err);
        res.status(500).json({
            success: false,
-           message: "Internal Server Error"
+           message: err.message
        });
    }
 });
