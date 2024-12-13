07810991b2c13fe96f017717b5be39129004dda6
shubham goyal <shubhamgoyal.dev@gmail.com>
2024-12-13 10:32:34 +0530

    fixes for server less

--- api_server/src/db/connectDb.js
+++ api_server/src/db/connectDb.js
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
 
+let MongoDbConnected = false;
+
 const connectDb = async () => {
+   if (MongoDbConnected) return;
    try {
       const connectionInstance = await mongoose.connect(
-         `${process.env.MONGODB_URI}/whatsapp`
+         `${process.env.MONGODB_URI}/${process.env.MONGO_DB}`
       );
       console.log(
          "mongoDb connection sucess: ",
          connectionInstance.connection.host
       );
+      if (connectionInstance.connection.host) {
+         MongoDbConnected = true;
+      }
    } catch (error) {
       console.log("mongoDb connection failed: ", error);
       process.exit(1);
