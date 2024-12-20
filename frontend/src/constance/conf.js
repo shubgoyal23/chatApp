49adbf52f5669450e4911ebebbc3b9a42be336da
shubham goyal <shubhamgoyal.dev@gmail.com>
2024-12-20 10:18:32 +0530

    test

--- frontend/src/constance/conf.js
+++ frontend/src/constance/conf.js
@@ -1,8 +1,3 @@
-// const conf = {
-//     WS_URL: "ws://localhost:3000",
-//     API_URL: "http://localhost:8000/api/v1",
-//     GIN_URL: "http://localhost:3000"
-// }
 const conf = {
    WS_URL: import.meta.env.VITE_WS_URL,
    API_URL: import.meta.env.VITE_API_URL,
